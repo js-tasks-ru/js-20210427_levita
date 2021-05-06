089a1bf5d1e5c102dc69fcced32b4fc72d95e2ad
Levitanos <n.levitin@amulex.ru>
2021-05-06 09:55:15 +0300

    add create-getter solution

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,29 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const keys = path.split('.');
 
+    return function (obj) {
+        let value;
+
+        function getProp(prop){
+            return value ? value[prop] : obj[prop]
+        }
+
+        for (let point of keys) {
+            value = getProp(point)
+        }
+        return value
+    }
 }
+
+
+
+// const product = {
+//   category: {
+//     title: "Goods"
+//   }
+// }
+// const getter = createGetter('category.title');
+
+// console.log(getter(product));
