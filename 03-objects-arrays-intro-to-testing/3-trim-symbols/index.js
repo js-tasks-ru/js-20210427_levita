73ea5b643dc1ec7ac3d35a5192a48118788e8bd6
Levitanos <n.levitin@amulex.ru>
2021-05-11 08:46:37 +0300

    3.3 task add

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if ( size === undefined ){ 
+        return string
+    }
 
+    if ( size === 0) {
+        return ''
+    }
+
+    let trim = [ ...string.split('')]
+    let key = trim[0];
+    let count = 0;
+    
+    return trim = trim.filter( item => {
+        if (item !== key) {
+            count = 1;
+            key = item;
+            return item
+        }
+        if ( count < size) {
+            key = item;
+            count ++;
+            return item
+        }
+    }).join('')
 }
