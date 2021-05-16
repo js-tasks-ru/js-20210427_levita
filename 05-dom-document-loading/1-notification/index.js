c92f187a0e4a8378ef64fab7e07fb42b465e15f6
Levitanos <n.levitin@amulex.ru>
2021-05-16 16:43:38 +0300

    notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
+    constructor(message = '', 
+    {
+        duration = 2000,
+        type = 'success'
+    } = {}){
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.render()
+    }
+    
+    render() {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="notification ${this.type}" style="--value:${this.duration/1000}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.message}
+                    </div>
+                </div>
+            </div>
+        `
+        this.element = div.firstElementChild;
+        console.log(this.element)
+    }
 
+    show(el) {
+        this.__proto__.element ? this.__proto__.element.remove() : null;
+        el = el ? el : document.body;
+        el.append(this.element);
+        setTimeout(() => {
+            this.remove()
+        }, this.duration);
+
+        this.__proto__.element = this.element;
+    }
+
+    remove() {
+        this.element.remove();
+        this.__proto__.element ? this.__proto__.element.remove() : null
+    }
+
+    destroy() {
+        this.element.remove();
+        this.__proto__.element ? this.__proto__.element.remove() : null
+    }
 }
