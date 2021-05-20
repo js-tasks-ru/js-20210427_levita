b09a89422f27376b93b43059a90367e4aa5f3231
Levitanos <n.levitin@amulex.ru>
2021-05-20 21:36:50 +0300

    tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,4 +1,55 @@
 class Tooltip {
+    element = null;
+    _instance = null
+    constructor(message = '') {
+        if(!Tooltip._instance)  Tooltip._instance = this;
+        this.instance = this;
+        this.message = message;
+        this.render()
+
+        return Tooltip._instance
+    }
+
+    addListeners(){
+        const divs = document.querySelectorAll('[data-tooltip]');
+        divs.forEach(item => {
+            item.addEventListener('pointerover', ev => this.renderTextTooltip(ev))
+            item.addEventListener('mousemove', ev => this.moveTooltip(ev))
+            item.addEventListener('pointerout', ev => this.clearTooltip(ev))
+        })
+    }
+    renderTextTooltip(ev) {
+        this.message = ev.target.dataset.tooltip;
+        this.element.textContent = this.message
+        document.body.append(this.element)
+    }
+
+    clearTooltip() {
+        this.message = '';
+        this.element.remove()
+    }
+    moveTooltip(ev) {
+        
+        const pageX = ev.clientX
+        const pageY = ev.clientY
+        this.element.style.top = `${pageY}px`
+        this.element.style.left = `${pageX}px`
+    }
+
+    render() {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="tooltip">${this.message}</div>
+        `
+        this.element = div.firstElementChild;
+    }
+    initialize() {
+        this.addListeners()
+    }
+
+    destroy() {
+        this.element.remove()
+    }
 
 }
 
