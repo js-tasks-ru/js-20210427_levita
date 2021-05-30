0027916e9950fe133933c534f949ab8b8e068b72
Levitanos <n.levitin@amulex.ru>
2021-05-30 14:53:13 +0300

    init

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,40 @@
 export default class SortableList {
+    element = null;
+    constructor(items = []) {
+        this.items = [...items.items];
+        this.render()
+        console.log(this)
+    }
+
+    initListeners() {
+        this.element.childNodes.forEach(item => {
+            item.addEventListener('mousedown', event => this.dragOn(event) )
+        })
+    }
+    
+    dragOn(event) {
+        const elem = event.currentTarget;
+        elem.classList.add('sortable-list__item_dragging')
+        elem.addEventListener('mouseup', this.dragOff)
+    }
+
+    dragOff = (event) => {
+        const elem = event.currentTarget;
+        console.log('s')
+        elem.classList.remove('sortable-list__item_dragging')
+        elem.removeEventListener('mouseup', this.dragOff)
+    }
+
+    render() {
+        const ul = document.createElement('ul');
+        ul.classList.add('sortable-list')
+        this.items.map(item => {
+            item.classList.add('sortable-list__item')
+            ul.append(item);
+        })
+        this.element = ul;
+        this.initListeners()
+    }
+
 
 }
